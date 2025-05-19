ec8cabd5d4554488dcfb6ff13b5b4e9f3a5fdc93
hysu <dame3902@gmail.com>
2025-05-19 13:55:57 -0700

    init

---
+++ balance.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define K 10        // 5, 10 ,100
+#define LMIN 11     // load limit
+#define LMAX 1000   
+#define DMIN 100    // distribution 
+#define DMAX 1000   
+#define MAX_CYCLES 1000000
+
+int main() {
+    
+}
